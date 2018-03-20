84502561c687367ca8e29ccfa4caeb8acbcf0d01
Desarrollo <Desarrollo@home>
2018-03-19 23:41:45 -0600

    tERMINADO

--- RH/src/rh/RH.java
+++ RH/src/rh/RH.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author Docente
+ * @author Francisco Ayala
  */
 public class RH {
 
@@ -18,17 +18,17 @@ public class RH {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        Empleado []emp;
+
+        Empleado[] emp;
         Scanner lea = new Scanner(System.in);
         System.out.println("Cuantos empleados");
-        int total =lea.nextInt();
+        int total = lea.nextInt();
         emp = new Empleado[total];
-        int contador =0;
+        int contador = 0;
         String nombre;
         double sueldo;
         //Operario tmp;
-        do{
+        do {
             System.out.println("Nombre del empleado");
             nombre = lea.next();
             System.out.println("Ingrese sueldo");
@@ -39,50 +39,70 @@ public class RH {
             int vacaciones;
             int horasextra;
             int regalias;
-            if(opc == 1){
+            if (opc == 1) {
                 System.out.println("Ingrese 1 si la empresa paga el carro, caso contrario cualquier otro numero");
                 carro = lea.nextInt();
                 System.out.println("Ingrese 1 si la empresa paga las vacaciones, caso contrario cualquier otro numero");
                 vacaciones = lea.nextInt();
-                emp[contador] = new Gerentes(nombre, sueldo,carro == 1,vacaciones == 1);   
-            }
-            else{
+                emp[contador] = new Gerentes(nombre, sueldo, carro == 1, vacaciones == 1);
+
+                //Gerentes con Bonos
+                Gerentes tmp2;
+                for (int i = 0; i < emp.length; i++) {
+                    if (emp[i] instanceof Gerentes) {
+                        tmp2 = (Gerentes) emp[i];
+                        if (tmp2.isTienecarro() == true );
+                        System.out.println(tmp2.getNombre() + " tiene Derecho a Carro ");
+                    } else  {
+                        System.out.println("  no tiene Derecho a Vehiculo ");
+
+                    }
+                }
+                
+                for (int i = 0; i < emp.length; i++) {
+                    if (emp[i] instanceof Gerentes) {
+                        tmp2 = (Gerentes) emp[i];
+                        if (tmp2.isVacacionespagadas() == true);
+                        System.out.println(tmp2.getNombre() + " tiene Vacaciones pagadas");
+                    } else {
+                        System.out.println(" no tiene Derecho a Vacaciones pagadas ");
+                    }
+
+                }
+
+            } else {
                 System.out.print("Cuantas horas extra tiene:");
-                horasextra =lea.nextInt();
+                horasextra = lea.nextInt();
                 System.out.println("Ingrese 1 si el empleado tiene regalias, caso contrario cualquier otro numero");
                 regalias = lea.nextInt();
-                emp[contador] = new Operario(nombre, sueldo, horasextra, regalias ==1);
+                emp[contador] = new Operario(nombre, sueldo, horasextra, regalias == 1);
             }
             contador++;
-        }while(contador != total);
-        
+        } while (contador != total);
+
         System.out.println("Sueldos");
-        
-        for(int i = 0; i<emp.length;i++){
-            System.out.println("nombre" + emp[i].getNombre()+ " ----------- Sueldo: " + emp[i].getSueldo());
+
+        for (int i = 0; i < emp.length; i++) {
+            System.out.println(" nombre " + emp[i].getNombre() + " ----------- Sueldo: " + emp[i].getSueldo());
         }
-        
+
         Operario tmp;
-        for (int i= 0; i<emp.length;i++){
-            if(emp[i] instanceof Operario){
+        for (int i = 0; i < emp.length; i++) {
+            if (emp[i] instanceof Operario) {
                 tmp = (Operario) emp[i];
-                if(tmp.getHoraextra() >=1);
-                System.out.println(tmp.getNombre() + "tiene" + tmp.getHoraextra()+ "horas extras");
+                if (tmp.getHoraextra() >= 1);
+                System.out.println(tmp.getNombre() + " tiene " + tmp.getHoraextra() + " horas extras");
             }
         }
         //Empleados con regalias 
-        //Operario tmp2;
-       for(int i=0; i<emp.length;i++){
-            if(emp[i] instanceof Operario ){
+        for (int i = 0; i < emp.length; i++) {
+            if (emp[i] instanceof Operario) {
                 tmp = (Operario) emp[i];
-                if(tmp.isRegalias());
-                   System.out.println(tmp.getNombre()+ " tiene Regalias");
-                }
+                if (tmp.isRegalias() == true);
+                System.out.println(tmp.getNombre() + " tiene Regalias");
             }
-       
-       
-        
-    }
-    
+        }
+
     }
 
+}
